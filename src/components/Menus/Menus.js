2099e39dc38d1f4bca9eabe072e39494f1a7a18f
ankitasri20 <ankitasrivastava2504@gmail.com>
2025-06-22 23:20:14 +0530

    first

---
+++ src/components/Menus/Menus.js
@@ -0,0 +1,111 @@
+import React from "react";
+import "./Menus.css";
+import profile from "../../Assets/profile_img.jpg";
+import { IoHome } from "react-icons/io5";
+import {
+  FcAbout,
+  FcBiotech,
+  FcBusinessContact,
+  FcReading,
+  FcVideoProjector,
+  FcVoicePresentation,
+} from "react-icons/fc";
+import { FcHome } from "react-icons/fc";
+import { FcBusinessman } from "react-icons/fc";
+
+const Menus = ({ toggle }) => {
+  return (
+    <>
+      {toggle ? (
+        <>
+          <div className="navbar-profile-pic">
+            <img src={profile} alt="profile pic" />
+          </div>
+          <div className="nav-items">
+            <div className="nav-item">
+              <div className="nav-link">
+                <FcHome />
+                Home
+              </div>
+
+              <div className="nav-link">
+                <FcAbout />
+                About
+              </div>
+
+              <div className="nav-link">
+                <FcBusinessman />
+                Work experience
+              </div>
+
+              <div className="nav-link">
+                <FcBiotech />
+                Tech Stack
+              </div>
+
+              <div className="nav-link">
+                <FcReading />
+                Education
+              </div>
+
+              <div className="nav-link">
+                <FcVideoProjector />
+                Projects
+              </div>
+
+              <div className="nav-link">
+                <FcVoicePresentation />
+                Testimonoal
+              </div>
+
+              <div className="nav-link">
+                <FcBusinessContact />
+                Contact
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+           <div className="nav-items">
+            <div className="nav-item">
+              <div className="nav-link">
+                <FcHome title="Home" />
+              </div>
+
+              <div className="nav-link">
+                <FcAbout  title="About" />
+              </div>
+
+              <div className="nav-link">
+                <FcBusinessman title="Work experience"/>
+              </div>
+
+              <div className="nav-link">
+                <FcBiotech  title="Tech Stack" />
+              </div>
+
+              <div className="nav-link">
+                <FcReading title=" Education" />
+              </div>
+
+              <div className="nav-link">
+                <FcVideoProjector title="Projects"/>
+              </div>
+
+              <div className="nav-link">
+                <FcVoicePresentation  title="Testimonoal"/>
+              </div>
+
+              <div className="nav-link">
+                <FcBusinessContact title="Contact"/>
+              </div>
+            </div>
+          </div> 
+        </>
+      )}
+    </>
+  );
+};
+
+export default Menus;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../pages/home.css"
+
+const home = () => {
+  return (
+    <>
+        <div className="home-container">
+          <div className="container home-content">
+            <h1>hii I'm a</h1>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default home
